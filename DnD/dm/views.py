f95df146ebb63e4f5a6757b5d001646fccbba2b6
User0605200841241 <110300711+Nevedomay-Hren@users.noreply.github.com>
2023-09-23 19:40:02 +0300

    update

--- DnD/dm/views.py
+++ DnD/dm/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def dm_index(request):
     return render(request, 'dm/dm_index.html')
 
+
 def players_index(request):
     return render(request, 'dm/players_index.html')
+
 
 def admin(request):
     return render(request, 'dm/admin.html')
 
+
 def clear_card(request):
     return render(request, 'dm/clear_card.html')
 
+
 def my_card(request):
     return render(request, 'dm/my_card.html')
+
 
 def groups(request):
     return render(request, 'dm/groups.html')
